10e91b18cdeeead8690cf19a6354657a9e458980
wesclicdev <wesclicdev@gmail.com>
2024-04-04 16:00:30 +0700

    Memmbuat flow display

--- AlgorithmaSearch.cpp
+++ AlgorithmaSearch.cpp
@@ -56,3 +56,10 @@ void LinearSearch() {
         cin >> ch;
     }while ((ch == 'y') || (ch == 'Y'));
 }
+
+void display() {
+   cout << "Array elements are: ";
+   for (i = 0; 1 < n; i++)
+    cout << arr[i] << " ";
+    cout << endl;  
+}
