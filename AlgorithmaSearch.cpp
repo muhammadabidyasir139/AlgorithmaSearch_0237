ea9e6b48cd8ae6f5f2a8fcba3ffa092c2b96c146
wesclicdev <wesclicdev@gmail.com>
2024-04-04 15:54:36 +0700

    Memmbuat step 4

--- AlgorithmaSearch.cpp
+++ AlgorithmaSearch.cpp
@@ -44,7 +44,9 @@ void LinearSearch() {
             if (arr[i] == item)
             {
                 cout << "\n" << item << "found at position" << (i + 1) << endl;//step 6
+                break;
             }
+            i++;
         }
     }
 }
